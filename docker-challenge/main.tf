74a50696042000642f562d661621bba5f640212d
andysolognier <andysolognier@proton.me>
2026-08-13 17:16:34 +0000

    terraform modules

---
+++ docker-challenge/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
+provider "docker" {
+# Explicitly points to the standard Ubuntu Docker socket
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "simplegoservice" {
+  name         = "registry.gitlab.com/alta3/simplegoservice"
+  keep_locally = true    // keep image after "destroy"
+}
+
+resource "docker_container" "simplegoservice" {
+  image = docker_image.simplegoservice.image_id
+  network_mode = "bridge"
+  name = var.container_name
+  ports {
+    internal = var.internal_port
+    external = var.external_port
+  }
+}
